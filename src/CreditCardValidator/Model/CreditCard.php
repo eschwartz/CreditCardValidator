348dd9e576d811e88e2437dd5cc8cd6ce5f2256c
Edan Schwartz <eschwartz@mindboxstudios.com>
2013-05-12 16:03:28 -0500

    * Test and build CreditCard number normalizer

--- src/CreditCardValidator/Model/CreditCard.php
+++ src/CreditCardValidator/Model/CreditCard.php
@@ -13,4 +13,14 @@ class CreditCard extends BaseModel {
   public function getNumber() {
     return $this->attributes['number'];
   }
+
+  /**
+   * Strips all non-numeric characters from
+   * the card number, including spaces and dashes
+   *
+   * @return float      Card number
+   */
+  public function getNumberNormalized() {
+    return (float) preg_replace("/[^0-9]/", "", $this->getNumber());
+  }
 }
--- src/CreditCardValidator/Tests/Model/CreditCardTest.php
+++ src/CreditCardValidator/Tests/Model/CreditCardTest.php
@@ -20,4 +20,11 @@ class CreditCardTest extends \PHPUnit_Framework_TestCase {
 
     $this->assertEquals("12345", $this->card->getNumber());
   }
+
+  public function testCanGetNormalizedCreditCardNumberAsFloat() {
+    $this->card->setNumber("1234-5678 90abcd gef hij12.34\\56//7890");
+    $normalized = $this->card->getNumberNormalized();
+    $this->assertEquals("12345678901234567890", $normalized);
+    $this->assertInternalType("float", $normalized);
+  }
 }
